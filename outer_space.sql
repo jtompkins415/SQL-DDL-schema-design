88d9cbd45536d0795ed25a9119956f6cb4a2c0a6
jtompkins415 <john.tompkins415@gmail.com>
2022-10-03 07:44:16 -0700

    add schema and update outer space tables and include seed data

--- outer_space.sql
+++ outer_space.sql
@@ -12,17 +12,40 @@ CREATE TABLE planets
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  galaxy  INT REFERENCES galaxies ON DELETE CASCADE,
+  moons INT[] REFERENCES moons ON DELETE CASCADE
+);
+
+CREATE TABLE galaxies (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  main_star TEXT NOT NULL,
+);
+
+CREATE TABLE moons (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  rotates_around INT REFERENCES planets ON DELETE CASCADE
 );
 
 INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+  (name, orbital_period_in_years, galaxy, moons)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('Earth', 1.00, 1, '{5}'),
+  ('Mars', 1.88, 1, '{2,4}'),
+  ('Venus', 0.62, 1,'{}'),
+  ('Neptune', 164.8, 1, '{1,4}'),
+  ('Proxima Centauri b', 0.03, 1,'{}'),
+  ('Gliese 876 b', 0.23, 1,'{}');
+
+INSERT INTO galaxies (name, main_star)
+VALUES
+('Milky Way', 'The Sun');
+
+INSERT INTO moons (name, rotates_around)
+VALUES
+('Naiad', 4),
+('Deimos', 2),
+('Triton', 4),
+('Phobos', 2),
+('The Moon', 1);
